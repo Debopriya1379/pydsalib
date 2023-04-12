d580540b9fb434dfc0032d7dfc2f163f070b4b42
Debopriya1379 <papai7909@gmail.com>
2023-04-12 10:57:26 +0530

    add two polynomials and multiply two polynomials created

--- polynomialRepLL.py
+++ polynomialRepLL.py
@@ -47,14 +47,85 @@ class PolynomialLL:
             temp=temp.next
         print()
 
-pl=PolynomialLL()
-pl.display()
-pl.add_term(8,2)
-pl.add_term(-7,0)
-pl.add_term(6,3)
-pl.add_term(5,4)
-pl.add_term(-2,5)
-pl.add_term(5,4)
-pl.add_term(6,3)
-pl.add_term(6,1)
-pl.display()
+class AddTwoPolynomial:
+    def __init__(self,poly1,poly2):
+        self.poly1=poly1
+        self.poly2=poly2
+    def add(self):
+        p1_temp=self.poly1.start
+        p2_temp=self.poly2.start
+        resPol=PolynomialLL()
+        while(p1_temp!=None and p2_temp!=None):
+            if(p1_temp.exp==p2_temp.exp):
+                resPol.add_term((p1_temp.coff + p2_temp.coff),p1_temp.exp)
+                p1_temp=p1_temp.next
+                p2_temp=p2_temp.next
+            elif(p1_temp.exp > p2_temp.exp):
+                resPol.add_term(p1_temp.coff,p1_temp.exp)
+                p1_temp=p1_temp.next
+            else:
+                resPol.add_term(p2_temp.coff,p2_temp.exp)
+                p2_temp=p2_temp.next
+        while(p1_temp!=None):
+            resPol.add_term(p1_temp.coff,p1_temp.exp)
+            p1_temp=p1_temp.next
+        while(p2_temp!=None):
+            resPol.add_term(p2_temp.coff,p2_temp.exp)
+            p2_temp=p2_temp.next
+        return resPol
+
+class MultiplyTwoPolynomials:
+    def __init__(self,p1,p2) -> None:
+        self.poly1=p1
+        self.poly2=p2
+    def multiply(self):
+        p1_temp=self.poly1.start
+        p2_temp=self.poly2.start
+        resPol=PolynomialLL()
+        while(p1_temp!=None):
+            while(p2_temp!=None):
+                resPol.add_term((p1_temp.coff*p2_temp.coff),p1_temp.exp+p2_temp.exp)
+                p2_temp=p2_temp.next
+            p1_temp=p1_temp.next
+            p2_temp=self.poly2.start
+        return resPol
+
+
+# pl1=PolynomialLL()
+# pl1.add_term(8,2)
+# pl1.add_term(-7,0)
+# pl1.add_term(6,3)
+# pl1.add_term(5,4)
+# pl1.add_term(5,4)
+# pl1.add_term(6,3)
+# pl1.add_term(6,1)
+# pl1.display()
+
+# pl2=PolynomialLL()
+# pl2.add_term(-7,0)
+# pl2.add_term(6,3)
+# pl2.add_term(5,4)
+# pl2.add_term(-2,5)
+# pl2.add_term(5,4)
+# pl2.add_term(6,3)
+# pl2.display()
+
+pl1=PolynomialLL()
+pl1.add_term(10,4)
+pl1.add_term(5,3)
+pl1.add_term(2,2)
+pl1.display()
+
+pl2=PolynomialLL()
+pl2.add_term(2,4)
+pl2.add_term(3,2)
+pl2.add_term(1,1)
+pl2.display()
+
+# res=AddTwoPolynomial(pl1,pl2)
+# resPolynomial=res.add()
+# resPolynomial.display()
+
+res=MultiplyTwoPolynomials(pl1,pl2)
+resPolynomial=res.multiply()
+resPolynomial.display()
